3266f7aee2e6cab2a664a6c042d5e4af9a0b1efe
Clàudia Martín <32127295+ClaudiaMartin00@users.noreply.github.com>
2018-02-15 13:22:27 +0100

    Add files via upload

---
+++ ForA2.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+print ("DIVISORES")
+num= int("Inserte numero")
+if num<0
+	print ("Le he pedido un número mayor que cero!!!")
+else:       
+For i in range(1,num+1)
+	if num%1==0:
+		print "Los divisores de", num "son:"
+		
---
+++ ForA3.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+print ("MAYORES QUE EL PRIMERO")
+num=int("Cuantos valores quieres introducir")
+if num>0	
+	print ("Imposible")
+
